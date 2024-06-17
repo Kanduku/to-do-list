2ce045e3aeb152f88eee93cacf07fbacb663b9b7
nagendra kumar <rocky.naggi@gmail.com>
2024-06-17 15:39:57 +0530

    fit

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -8,3 +8,17 @@ export async function POST(request){
      await Topic.create({title ,description});
      return NextResponse.json({message:"topic created"},{status:201});
 }
+export async function GET(request){
+    
+     await connectMongoDB();
+     const topics = await Topic.find();
+ 
+     return NextResponse.json({ topics});
+}
+export async function DELETE(request){
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongoDB();
+    
+await Topic.findByIdAndDelete(id);
+return NextResponse.json({message:"topic deleted"},{status:200});
+}
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,21 +2,40 @@ import Removebtn from "./Removebtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList () {
+const getTopics = async() => {
+    try{
+        const res =await fetch("http://localhost:3000/api/topics",{
+            cache:"no-store",
+        });
+        if (!res.ok){
+            throw new Error("Failed to fetch topics");
+        }
+        return res.json();
+    }
+    catch(error){
+        console.log("error loading topics", error);
+    }
+}
+
+
+export default async function TopicsList () {
+    const {topics} =await getTopics();
     return(
         <>
+        {topics.map(t =>(
         <div className="border border-slate-200 my-3 p-4 flex justify-between gap-5 items-start">
         <div >
-            <h2 className="font-bold text-2xl">topic name</h2>
-            <div>topic description</div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
         </div>
             <div className="flex gap-2">
-                <Removebtn />
-                <Link href={"/EditTopic/123"}> 
+                <Removebtn id={t._id} />
+                <Link href={`/EditTopic/${t._id}`}> 
                 <HiPencilAlt  size ={24} />
                 </Link>
             </div>
         </div>
+        ))}
         </>
 
     );
